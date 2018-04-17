ef87b5e3bbea661d0f07c4cddb39f05408f34bbd
plus-x <carlos@plu.sx>
2018-04-17 12:01:25 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/is7xazgpo8/wp_woocommerce_tax_rate_locations.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_woocommerce_tax_rate_locations` (  `location_id` bigint(20) NOT NULL AUTO_INCREMENT,  `location_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,  `tax_rate_id` bigint(20) NOT NULL,  `location_type` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL,  PRIMARY KEY (`location_id`),  KEY `tax_rate_id` (`tax_rate_id`),  KEY `location_type` (`location_type`),  KEY `location_type_code` (`location_type`,`location_code`(90))) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40000 ALTER TABLE `wp_woocommerce_tax_rate_locations` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_woocommerce_tax_rate_locations` ENABLE KEYS */;
