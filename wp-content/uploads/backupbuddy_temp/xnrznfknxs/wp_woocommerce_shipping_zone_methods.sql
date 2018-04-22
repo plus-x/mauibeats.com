977c773db70bbec67b8e1db711ff36b7a5913e6b
plus-x <carlos@plu.sx>
2018-04-22 12:01:03 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/xnrznfknxs/wp_woocommerce_shipping_zone_methods.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_woocommerce_shipping_zone_methods` (  `zone_id` bigint(20) NOT NULL,  `instance_id` bigint(20) NOT NULL AUTO_INCREMENT,  `method_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,  `method_order` bigint(20) NOT NULL,  `is_enabled` tinyint(1) NOT NULL DEFAULT '1',  PRIMARY KEY (`instance_id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40000 ALTER TABLE `wp_woocommerce_shipping_zone_methods` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_woocommerce_shipping_zone_methods` ENABLE KEYS */;
