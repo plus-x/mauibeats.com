5de899b4771f0859ca2b676d03e5e483d883fe98
plus-x <carlos@plu.sx>
2018-04-27 12:01:04 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/c8idsjrnou/wp_aiowps_login_lockdown.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_aiowps_login_lockdown` (  `id` bigint(20) NOT NULL AUTO_INCREMENT,  `user_id` bigint(20) NOT NULL,  `user_login` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,  `lockdown_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',  `release_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',  `failed_login_ip` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',  `lock_reason` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',  `unlock_key` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',  PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40000 ALTER TABLE `wp_aiowps_login_lockdown` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_aiowps_login_lockdown` ENABLE KEYS */;
