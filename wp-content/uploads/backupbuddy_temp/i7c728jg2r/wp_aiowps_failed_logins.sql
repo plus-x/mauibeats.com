e6509a17301f588066cc52ea7b18649e89c7e811
plus-x <carlos@plu.sx>
2018-05-01 12:01:04 -0500

    auto update

--- wp-content/uploads/backupbuddy_temp/i7c728jg2r/wp_aiowps_failed_logins.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `wp_aiowps_failed_logins` (  `id` bigint(20) NOT NULL AUTO_INCREMENT,  `user_id` bigint(20) NOT NULL,  `user_login` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,  `failed_login_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',  `login_attempt_ip` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',  PRIMARY KEY (`id`)) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40000 ALTER TABLE `wp_aiowps_failed_logins` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wp_aiowps_failed_logins` ENABLE KEYS */;
